5c0c8e13c676a3c52a5fb7bdaff5c2dfd9c5381d
thuy <kieuthuy10a2@gmail.com>
2020-10-11 21:18:48 +0700

    commit code

--- java/1. helloworl-xay dung moi truong/Learn Code/src/Helloworld.java
+++ java/1. helloworl-xay dung moi truong/Learn Code/src/Helloworld.java
@@ -1,10 +1,27 @@
-
+/*
+ * Helloworld <tenclass, tenfile>
+ * 
+ * Helloworld programming <mota file>
+ *
+ * @author ThuyKT <ten nguoi code>
+ */
 public class Helloworld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("Hello World");
-
+		print("Helloworld");
 	}
 
+	/*
+	 * In dong chu len console
+	 * 
+	 * @param txtName ten dong code muon in ra
+	 * @param txtName ten dong code muon in ra
+	 * @param txtName ten dong code muon in ra
+	 *
+	 * @return void
+	 */
+	public static void print(String txtName) {
+		// in ra console
+		System.out.print(txtName);
+	}
 }
